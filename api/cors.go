5c80bd28a449dbdf7cae40b159d31dd0eddb205e
Daniel Middlecote <daniel@ometria.com>
2020-08-26 13:16:50 +0100

    Improved CORS middleware support

--- api/cors.go
+++ api/cors.go
@@ -6,19 +6,23 @@ import (
 	"github.com/rs/cors"
 )
 
-// DefaultCorsMW returns a middleware that adds Cross Origin Resource Sharing
-// support for all origins for GET and POST methods.
+// CorsMiddleware is a function designed to run some code before and/or after
+// another Handler, related to Cross Origin Resource Sharing.
+type CorsMiddleware func(http.Handler) http.Handler
+
+// DefaultCorsMW returns a cors middleware that adds support for all origins
+// for GET and POST methods.
 // This middleware must be used in combination with an OPTIONS method endpoint.
-func DefaultCorsMW() Middleware {
+func DefaultCorsMW() CorsMiddleware {
 	return func(next http.Handler) http.Handler {
 		return cors.Default().Handler(next)
 	}
 }
 
-// AllowAllCorsMW returns a middleware that adds Cross Origin Resource Sharing
-// support for all origins, for all methods, with any header or credential.
+// AllowAllCorsMW returns a cors middleware that adds support for all origins,
+// for all methods, with any header or credential.
 // This middleware must be used in combination with an OPTIONS method endpoint.
-func AllowAllCorsMW() Middleware {
+func AllowAllCorsMW() CorsMiddleware {
 	return func(next http.Handler) http.Handler {
 		return cors.AllowAll().Handler(next)
 	}
